829414a4dc4689ccf232321de7603761cb46b924
Trevor Von Bruenchenhein <trevor@Trevors-MacBook-Pro.local>
2025-05-08 21:21:06 -0700

    init commit

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "App name",
+  description:
+    "App description",
+  url: "",
+  ogImage: "https://tx.shadcn.com/og.jpg",
+  links: {
+    twitter: "",
+    github: "",
+  },
+}
